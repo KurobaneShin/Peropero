081c054ecdaf97ea90082c2b642c87cb0c39b9a2
kurobane <darkflamemaster663@gmail.com>
2024-01-18 17:29:33 -0300

    fix: add envs

--- app/infra/supabase.ts
+++ app/infra/supabase.ts
@@ -1,13 +1,15 @@
 import { createClient } from "@supabase/supabase-js"
 import { Database } from "database.types"
-const supabaseUrl = "http://localhost:54321"
+
+const supabaseUrl = process.env.SUPABASE_URL
 if (!process.env.ANON_KEY) throw "SUPABASE_KEY is not set"
+if (!supabaseUrl) throw "SUPABASE_URL is not set"
 
 const supabaseKey = process.env.ANON_KEY
 // console.log(supabaseKey);
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
 
-const serviceRoleKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU"
+const serviceRoleKey = process.env.SERVICE_ROLE_KEY
+if (!serviceRoleKey) throw "SERVICE_ROLE_KEY is not set"
 
 export const superSupabase = createClient<Database>(supabaseUrl, serviceRoleKey)
