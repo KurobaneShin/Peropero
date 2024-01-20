7bd801200567f1e97ab2a5382b7c4632d4fb635e
kurobane <darkflamemaster663@gmail.com>
2024-01-20 15:52:51 -0300

    add new tables

---
+++ supabase/migrations/20240120181730_new_manga_relations.sql
@@ -0,0 +1,55 @@
+create table groups (
+  id serial primary key,
+  title text not null,
+  created_at timestamptz not null default now()
+);
+
+create table mangas_groups (
+  manga integer not null references mangas(id),
+  groupId integer not null references groups(id)
+);
+
+create table parodies (
+  id serial primary key,
+  title text not null,
+  created_at timestamptz not null default now()
+);
+
+create table mangas_parodies (
+  manga integer not null references mangas(id),
+  parody integer not null references parodies(id)
+);
+
+create table languages (
+  id serial primary key,
+  title text not null,
+  created_at timestamptz not null default now()
+);
+
+create table mangas_languages (
+  manga integer not null references mangas(id),
+  language integer not null references parodies(id)
+);
+
+create table characters (
+  id serial primary key,
+  name text not null,
+  created_at timestamptz not null default now()
+);
+
+create table mangas_characters (
+  manga integer not null references mangas(id),
+  characterId integer not null references characters(id)
+);
+
+create table profiles (
+  username text not null unique,
+  id uuid primary key not null unique references auth.users on delete cascade,
+  email text not null
+);
+
+create table favorites (
+  id serial primary key,
+  manga integer not null references mangas(id),
+  profile uuid not null references profiles(id)
+);
