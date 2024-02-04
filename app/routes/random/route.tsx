99d0ba6952e6c368c30b2bde60462341d4752472
icaro <darkflamemaster663@gmail.com>
2024-02-04 16:27:59 -0300

    create random route

---
+++ app/routes/random/route.tsx
@@ -0,0 +1,20 @@
+import { redirect } from "@remix-run/node"
+import { supabase } from "~/infra/supabase"
+
+export const loader = async () => {
+	const { count } = await supabase.from("mangas").select("*", {
+		count: "exact",
+		head: true,
+	})
+
+	const randomId = Math.floor(Math.random() * count!) + 1
+
+	const { data } = await supabase
+		.from("mangas")
+		.select("*")
+		.limit(1)
+		.eq("id", randomId)
+		.maybeSingle()
+
+	return redirect(`/mangas/${data?.id}`)
+}
