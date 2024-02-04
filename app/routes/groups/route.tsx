bf78cf2eb2ff37b775735b455a2d467679910f8b
icaro <darkflamemaster663@gmail.com>
2024-02-04 10:45:24 -0300

    better group page ui

--- app/routes/groups/route.tsx
+++ app/routes/groups/route.tsx
@@ -1,4 +1,15 @@
 import { Link, Outlet, useLoaderData, useLocation } from "@remix-run/react"
+import Page from "~/components/custom/Page"
+import { Button } from "~/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card"
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "~/components/ui/table"
 import { supabase } from "~/infra/supabase"
 
 export const loader = async () => {
@@ -18,34 +29,42 @@ export default function Groups() {
 	const { pathname } = useLocation()
 
 	return (
-		<div>
-			<div className="flex items-center justify-between">
-				<h1>Groups</h1>
-				{!pathname.includes("new") && (
-					<div>
-						<Link to="new">Add</Link>
+		<Page>
+			<Card>
+				<CardHeader className="space-y-1">
+					<div className="flex items-center justify-between">
+						<CardTitle className="text-2xl">Groups</CardTitle>
+						{!pathname.includes("new") && (
+							<Link to="new">
+								<Button>New Group</Button>
+							</Link>
+						)}
 					</div>
-				)}
-			</div>
+				</CardHeader>
+				<CardContent>
+					<Outlet />
 
-			<Outlet />
-
-			<table className="mt-4">
-				<thead>
-					<tr>
-						<th>Nome</th>
-						<th>Obras</th>
-					</tr>
-				</thead>
-				<tbody>
-					{groups.map((group) => (
-						<tr key={group.id}>
-							<td>{group.title}</td>
-							<td>{group.mangas_groups.length}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+					<Table>
+						<TableHeader>
+							<TableRow>
+								<TableHead className="w-[100px]">Name</TableHead>
+								<TableHead>Mangas</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							{groups.map((group) => (
+								<TableRow key={group.id}>
+									<TableCell>{group.title}</TableCell>
+									<TableCell>{group.mangas_groups.length}</TableCell>
+									<TableCell className="text-right">
+										<Link to={`/groups/${group.id}`}>Visit</Link>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</CardContent>
+			</Card>
+		</Page>
 	)
 }
