fc8366da5d33b77b55788c3ba2895d9490d5a738
kurobane <darkflamemaster663@gmail.com>
2024-02-13 14:52:45 -0300

    create more tests to authors new

--- app/routes/authors.new/route.spec.ts
+++ app/routes/authors.new/route.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from "bun:test"
-import { loader } from "./route"
+import { supabase } from "~/infra/supabase"
+import { action, loader } from "./route"
 
 describe("authors.new", () => {
 	test("if route is authenticated", async () => {
@@ -11,5 +12,41 @@ describe("authors.new", () => {
 			const location = (e as Response).headers.get("location")
 			expect(location).toEqual("/signin")
 		}
+	})
+
+	test("post is validated correctly", async () => {
+		const body = new URLSearchParams({
+			name: "test",
+		})
+		const request = new Request("localhost:3000/authors/new", {
+			method: "POST",
+			body,
+		})
+		const response = await action({ request, context: {}, params: {} })
+		expect(response).toBeInstanceOf(Response)
+		const location = (response as Response).headers.get("location")
+
+		const lastInserted = await supabase
+			.from("authors")
+			.select("*")
+			.order("id", { ascending: false })
+			.limit(1)
+			.maybeSingle()
+
+		expect(lastInserted.data?.name).toEqual("test")
+		if (!lastInserted.data?.id) throw new Error("No id found")
+		await supabase.from("authors").delete().eq("id", lastInserted.data?.id)
+
+		expect(location).toEqual("/new")
+	})
+
+	test("post is validated correctly", async () => {
+		const body = new URLSearchParams({})
+		const request = new Request("localhost:3000/authors/new", {
+			method: "POST",
+			body,
+		})
+		const response = await action({ request, context: {}, params: {} })
+		expect(response).toHaveProperty("errors.name")
 	})
 })
